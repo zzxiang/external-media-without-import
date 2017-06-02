13e42a91564501d55b8ad2ef1016ee05647a656e
zzxiang <zzxiang@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2017-06-02 06:13:09 +0000

    Added POST request and handling.
    git-svn-id: https://plugins.svn.wordpress.org/external-media-without-import/trunk@1669243 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- external-media-without-import.js
+++ external-media-without-import.js
@@ -1,9 +1,22 @@
 jQuery(function ($) {
   $('body').on('click', '.emwi-in-upload-ui button', function (e) {
     $('#emwi-media-new-panel').show();
+
+    $('.uploader-inline #emwi-add').click(function (e) {
+      var url = $('#emwi-url').val();
+      wp.media.post('add_external_media_without_import', { url: url })
+        .done(function (response) {
+          $('#emwi-error').hide();
+          console.error('add done! ' + response);
+        }).fail(function (response) {
+          $('#emwi-error').show().text(response.responseJSON.data);
+        });
+    });
+
     $('.uploader-inline #emwi-cancel').click(function (e) {
       $('#emwi-media-new-panel').hide();
     });
+
     e.preventDefault();
   });
 });
--- external-media-without-import.php
+++ external-media-without-import.php
@@ -15,7 +15,10 @@ wp_register_script( $script, $js_file, array( 'jquery' ) );
 wp_enqueue_script( $script );
 
 add_action( 'admin_menu', 'WP_ExternalMedia_WithoutImport\add_submenu' );
-add_action( 'post-plupload-upload-ui', 'WP_ExternalMedia_WithoutImport\post_plupload_upload_ui' );
+add_action( 'post-plupload-upload-ui', 'WP_ExternalMedia_WithoutImport\post_upload_ui' );
+add_action( 'post-html-upload-ui', 'WP_ExternalMedia_WithoutImport\post_upload_ui' );
+add_action( 'wp_ajax_add_external_media_without_import', 'WP_ExternalMedia_WithoutImport\wp_ajax_add_external_media_without_import' );
+add_action( 'admin_post_add_external_media_without_import', 'WP_ExternalMedia_WithoutImport\admin_post_add_external_media_without_import' );
 
 function add_submenu() {
     add_submenu_page(
@@ -28,7 +31,7 @@ function add_submenu() {
     );
 }
 
-function post_plupload_upload_ui() {
+function post_upload_ui() {
     $media_library_mode = get_user_option( 'media_library_mode', get_current_user_id() );
 ?>
     <div class="emwi-in-upload-ui">
@@ -40,9 +43,9 @@ function post_plupload_upload_ui() {
           <button class="button button-large">
             <?php echo __('Add External Media without Import'); ?>
           </button>
-          <?php print_media_new_panel( false ); ?>
+          <?php print_media_new_panel( true ); ?>
         <?php else : ?>
-          <a class="button button-large" href="<?php echo admin_url( '/upload.php?page=add-external-media-without-import', __FILE__ ); ?>">
+          <a class="button button-large" href="<?php echo esc_url( admin_url( '/upload.php?page=add-external-media-without-import', __FILE__ ) ); ?>">
             <?php echo __('Add External Media without Import'); ?>
           </a>
         <?php endif; ?>
@@ -52,22 +55,78 @@ function post_plupload_upload_ui() {
 }
 
 function print_submenu_page() {
-    print_media_new_panel( true );
+?>
+    <form action="<?php echo esc_url( admin_url('admin-post.php') ); ?>" method="post">
+      <?php print_media_new_panel( false ); ?>
+    </form>
+<?php
 }
 
-function print_media_new_panel( $is_in_submenu_page ) {
+function print_media_new_panel( $use_js ) {
 ?>
-    <div id="emwi-media-new-panel" <?php if ( !$is_in_submenu_page  ) : ?>style="display: none"<?php endif;?>>
+    <div id="emwi-media-new-panel" <?php if ( $use_js  ) : ?>style="display: none"<?php endif; ?>>
       <div class="url-row">
         <label><?php echo __('Add a media from URL'); ?></label>
         <span id="emwi-url-input-wrapper">
-          <input type="url" placeholder="<?php echo __('Image URL');?>">
+          <input id="emwi-url" name="url" type="url" placeholder="<?php echo __('Image URL');?>" value="<?php echo urldecode( $_GET['url'] ); ?>">
         </span>
       </div>
+      <div id="emwi-error" <?php if ( $use_js || empty( $_GET['error'] ) ) : ?>style="display: none"<?php endif; ?>>
+        <?php echo urldecode( $_GET['error'] ); ?>
+      </div>
       <div class="buttons-row">
-        <input type="button" id="emwi-add" class="button button-primary" value="<?php echo __('Add') ?>">
+        <input type="hidden" name="action" value="add_external_media_without_import">
+        <input type="submit" id="emwi-add" class="button button-primary" value="<?php echo __('Add') ?>">
         <input type="button" id="emwi-cancel" class="button" value="<?php echo __('Cancel') ?>">
       </div>
     </div>
 <?php
 }
+
+function wp_ajax_add_external_media_without_import() {
+    $error = add_external_media_without_import();
+    if ( empty( $error ) ) {
+        wp_send_json_success( 'lalala' );
+    }
+    else {
+        wp_send_json_error( $error, 500 );
+    }
+}
+
+function admin_post_add_external_media_without_import() {
+    $error = add_external_media_without_import();
+    $redirect_url = 'upload.php';
+    if ( !empty( $error ) ) {
+        $redirect_url = $redirect_url .  '?page=add-external-media-without-import&url=' . urlencode( $_POST['url'] ) . '&error=' . urlencode( $error );
+    }
+	wp_redirect( admin_url( $redirect_url ) );
+    exit;
+}
+
+function add_external_media_without_import() {
+    $url = $_POST['url'];
+    if ( is_callable( 'curl_init' ) ) {
+        $ch = curl_init( $url );
+        curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+        curl_setopt( $ch, CURLOPT_NOBODY, true );
+        curl_exec( $ch );
+        $file_type = curl_getinfo( $ch, CURLINFO_CONTENT_TYPE );
+        curl_close( $ch );
+        error_log("ZZXIANG content: $file_contents");
+        error_log("ZZXIANG MIME-TYPE: $file_type");
+    }
+    else {
+        return _( "The php of your server doesn't have cURL support enabled, Please contact the server administrator." );
+    }
+
+    /*
+    $attach_data = wp_generate_attachment_metadata( 572, 'http://i1266.photobucket.com/albums/jj532/zzxiang/Blog/2017-04-27-uncharted4-01_zpsewsha82u.jpeg' );
+    ob_start(); // start buffer capture
+    var_dump( $attach_data ); // dump the values
+    $contents = ob_get_contents(); // put the buffer into a variable
+    ob_end_clean(); // end capture
+    error_log( "[ZZXIANG DEBUG] $contents" ); // log contents of the result of var_dump( $object )
+     */
+    return "";
+}
+
