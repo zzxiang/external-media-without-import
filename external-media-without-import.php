21223f91116a50ef4bd3d13b59a432b67341d70b
zzxiang <zzxiang@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2017-06-02 06:12:16 +0000

    Distinguished between grid mode and list mode in upload page.
    git-svn-id: https://plugins.svn.wordpress.org/external-media-without-import/trunk@1669240 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- external-media-without-import.php
+++ external-media-without-import.php
@@ -12,14 +12,21 @@ wp_enqueue_style( $style );
 add_action( 'post-plupload-upload-ui', 'WP_ExternalMedia_WithoutImport\post_plupload_upload_ui' );
 
 function post_plupload_upload_ui() {
+    $media_library_mode = get_user_option( 'media_library_mode', get_current_user_id() );
 ?>
     <div class="external-media-without-import-button-wrapper" style="margin-top: 20px">
       <?php echo __('or'); ?>
     </div>
     <div class="external-media-without-import-button-wrapper">
-      <button class="button button-large">
-        <?php echo __('Add External Media without Import'); ?>
-      </button>
+      <?php if ( 'grid' === $media_library_mode ) : ?>
+        <button class="button button-large">
+          <?php echo __('Add External Media without Import'); ?>
+        </button>
+      <?php else : ?>
+        <a class="button button-large" href="<?php echo plugins_url( '/media-add.php', __FILE__ ); ?>">
+          <?php echo __('Add External Media without Import'); ?>
+        </a>
+      <?php endif; ?>
     </div>
 <?php
 }
