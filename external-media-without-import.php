5d24fe3288b53d0cd762b234a08608dc75719d5c
zzxiang <zzxiang@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2017-06-02 06:12:04 +0000

    Add style for dom elements added in upload panel.
    git-svn-id: https://plugins.svn.wordpress.org/external-media-without-import/trunk@1669239 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- external-media-without-import.php
+++ external-media-without-import.php
@@ -9,10 +9,13 @@ $css_file = plugins_url( '/external-media-without-import.css', __FILE__ );
 wp_register_style( $style, $css_file );
 wp_enqueue_style( $style );
 
-add_action( 'pre-plupload-upload-ui', 'WP_ExternalMedia_WithoutImport\pre_plupload_upload_ui' );
+add_action( 'post-plupload-upload-ui', 'WP_ExternalMedia_WithoutImport\post_plupload_upload_ui' );
 
-function pre_plupload_upload_ui() {
+function post_plupload_upload_ui() {
 ?>
+    <div class="external-media-without-import-button-wrapper" style="margin-top: 20px">
+      <?php echo __('or'); ?>
+    </div>
     <div class="external-media-without-import-button-wrapper">
       <button class="button button-large">
         <?php echo __('Add External Media without Import'); ?>
